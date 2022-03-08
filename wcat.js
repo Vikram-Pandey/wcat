1b80b861a48aa071a83643a92b5ea6b8f65f5474
Vikram <pandey.vikram@outlook.com>
2022-03-08 10:40:00 -0600

    Beginning of wcat Project

---
+++ wcat.js
@@ -0,0 +1,7 @@
+//cat command
+//node wcat.js [option] [filepath]
+//1: node wcat.js filepath-> display content of a file
+//2: node wcat.js filepath1 filepath2 -> display contents of all files
+//3: node wcat.js -> filepath : this will remove spaces in between multiple lines
+//-n -> gives numbering to all lines
+//-b -> gives numbering to only lines with data.
