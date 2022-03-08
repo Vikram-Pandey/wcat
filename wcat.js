adbc09c5047334ef59e06efe84ca423be38e9715
Vikram <pandey.vikram@outlook.com>
2022-03-08 11:20:35 -0600

    files input and read content commands created

--- wcat.js
+++ wcat.js
@@ -5,3 +5,42 @@
 //3: node wcat.js -> filepath : this will remove spaces in between multiple lines
 //-n -> gives numbering to all lines
 //-b -> gives numbering to only lines with data.
+
+//1) node wcat.js filepath=> display the contents of the file in a terminal
+const fs=require("fs");
+let inputArray=process.argv.slice(2);
+//console.log(inputArray);
+
+let optionsArr=[];
+
+let filesArr=[];
+
+//Added files path in file array
+
+for(let i=0;i<inputArray.length;i++){
+    filesArr.push(inputArray[i]);
+}
+
+//console.log(filesArr);
+
+//Check if the file exist 
+
+for(let j=0;j<filesArr.length;j++){
+    let doesExist=fs.existsSync(filesArr[j]);
+
+    if(!doesExist){
+        console.log("File does not exist");
+        return;
+    }
+}
+
+
+let content="";
+
+for(let i=0;i<filesArr.length;i++){
+    let filecontent=fs.readFileSync(filesArr[i]);
+
+    content+=filecontent+"\n";
+}
+
+console.log(content);
